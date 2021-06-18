b5bef64a1c56a29bdf446c4b4790fc2aa3958e44
renudharshak <svjayam5@gmail.com>
2021-06-18 16:01:19 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".popular-category").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  adaptiveHeight: true,
+});
+
+$(".banner").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
